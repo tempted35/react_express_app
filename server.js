168aad4fb64ce4f2cf34d004b7ef9dcac685cf4f
tempted35 <g35dave@gmail.com>
2019-12-27 17:11:56 -0800

    Add user authentication

--- server.js
+++ server.js
@@ -2,9 +2,37 @@ const URL = "mongodb://localhost:27017";
 
 const mongoClient = require('mongodb').MongoClient
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const app = express();
+app.use(express.json());
 //const router = express.Router();
+
+//------------------------------------------------------------------------------
+// ROUTES
+//------------------------------------------------------------------------------
+const users = [{name: 'Dave'},  {name: 'Charlotte', age: 3}]
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', async (req, res, next) => {
+  try
+  {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // console.log(salt);
+    // console.log(hashedPassword);
+    const user = {name: req.body.name, password: hashedPassword}
+    console.log(user);
+    res.status(201).send()
+  }
+  catch(e)
+  {
+    console.log(e);
+    res.status(500).send();
+  }
+});
 
 //------------------------------------------------------------------------------
 // ROUTES
